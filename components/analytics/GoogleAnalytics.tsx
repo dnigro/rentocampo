4c09af06f513cfc31b8af9533cddb221f62e14c1
Dan T <dantenigro@gmail.com>
2026-09-20 19:38:48 -0300

    Activate GA4 for RentoCampo

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -11,7 +11,7 @@ declare global {
   }
 }
 
-const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+const GA_ID = "G-PFGVKEFPCJ";
 
 export default function GoogleAnalytics() {
   const pathname = usePathname();
